bf0161a28856297a1d19da158e4a8230bbc183b5
Melissa Cadavid <mcadavid@qualit.tech>
2018-01-11 11:57:40 -0500

    Crear variables, funciones e importar el tipo de dato Empleado desde un componente

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,11 +1,26 @@
 import {Component} from '@angular/core';
+import {Empleado} from './empleado';
 
 @Component({
     selector: 'empleado-tag',
     templateUrl: './empleado.component.html'
 })
 export class EmpleadoComponent{
-    public titulo = 'Titulo del componente empleado';
+    public titulo = 'Componente empleados';
+    public empleado:Empleado;
+    public trabajadores:Array<Empleado>;
 
-    
+    constructor(){
+        this.empleado= new Empleado('Melissa',33,'Desarrolladora',true);
+        this.trabajadores = [
+            new Empleado('Melissa',33,'Desarrolladora',true),
+            new Empleado('Manolo',40,'Administrativo',true),
+            new Empleado('Ana',30,'Cocinera',false)
+        ];
+    }
+
+    ngOnInit(){
+        console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
 }
--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -11,6 +11,51 @@ import {Component} from '@angular/core';
 })
 export class FrutaComponent{
     //definir propiedades
-    public nombre_componente = 'Componente de fruta';
+    public nombre_componente = 'Componente de fruta'; //valor predefinido
     public listado_componente = 'Naranja, Manzana, Pera y Sandia';
+    public nombre:string = "Melissa Cadavid";
+    public edad:number = 30;
+    public mayorDeEdad: boolean = true;
+    public trabajos:Array<string> = ['Pintor','Desarrollador'];
+    comodin:any = 'Cualquier cosa';
+    ejemplo:string;
+
+
+    constructor(){
+        //para darle valor a las propiedades
+        console.log(this.comodin);
+        console.log(this.trabajos);
+        this.ejemplo = 'ejemplooooo'; //asignar valor dinamico en el constructor (buenas practicas)
+
+        //this.holaMundo(this.nombre);
+        //this.cambiarNombre();
+        //alert(this.nombre);
+    }
+
+    ngOnInit(){
+        //las funciones se deben llamar en este método (buenas practicas) no en el constructor
+        this.holaMundo(this.nombre);
+        this.cambiarNombre();
+        console.log(this.nombre);
+
+        //variables y alcance
+        var uno = 8;
+        var dos = 15;
+
+        if (uno === 8){
+            let uno = 3;//funciona a nivel del bloque
+            var dos = 88;//el valor va a ser global, dentro y fuera siempre sera el mismo valor
+
+            console.log('DENTRO DEL IF' + uno + " " + dos);
+        }
+        console.log('FUERA DEL IF' + uno + " " + dos);
+    }
+
+    holaMundo(nombre){
+        alert('Hola mundo!!' + nombre);
+    }
+
+    cambiarNombre(){
+        this.nombre = 'Juan Lopez';
+    }
 }
