1429ee5bdafead9f703f5aa428e9453fabf651eb
Melissa Cadavid <mcadavid@qualit.tech>
2018-01-18 10:11:01 -0500

    Servicios http y ajax

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,20 +1,40 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
+import { PeticionesService } from '../services/peticiones.service';
+import { NgModuleCompileResult } from '@angular/compiler/src/ng_module_compiler';
 
 @Component({
     selector: 'coches',
-    templateUrl: './coches.component.html'
+    templateUrl: './coches.component.html',
+    providers: [PeticionesService]
 })
 export class CochesComponent{
     public coche: Coche;
     public coches:Array<Coche>;
+    public articulos;
 
-    constructor(){
+    constructor(private _peticionesService: PeticionesService){
         this.coche = new Coche("","","");
         this.coches = [
             new Coche("Seat Panda","120","Blanco"),
             new Coche("Renault Clio","110","Azul")
         ]
+    }
+
+    ngOnInit(){
+        //console.log(this._peticionesService.getPrueba());
+        this._peticionesService.getArticulos().subscribe(
+            result => {//funcion callback de flecha
+                this.articulos = result;
+                if (!this.articulos){
+                    console.log("Error en el servidor");
+                }
+            },
+            error => {//funcion callback de flecha
+                var errorMessage = <any>error;
+                console.log(errorMessage);
+            }
+        );
     }
 
     onSubmit(){
---
+++ src/app/services/peticiones.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'; //observable
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class PeticionesService{
+    public url: string;
+
+    constructor(private _http: Http){
+        this.url = "https://jsonplaceholder.typicode.com/posts";
+    }
+
+    getPrueba(){
+        return 'Hola mundo desde el servicio.';
+    }
+
+    getArticulos(){
+        return this._http.get(this.url)
+                         .map(res => res.json());//convertir respuesta a json xa reutilizar
+    }
+    
+}
